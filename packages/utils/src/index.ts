6468a9c236a30f3650ca0a218055ac7de359b84f
Jared Palmer <jaredloganpalmer@gmail.com>
2020-09-23 18:41:52 -0400

    Add phone utils (#4)

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,7 @@
 /**
  * Return a slugified copy of a string.
  *
- * @param {string} str The string to be slugified
- * @return {string} The slugified string.
+ * @param string - The string to be slugified
  */
 export function toSlug(str: string): string {
   let s = str;
@@ -16,4 +15,33 @@ export function toSlug(str: string): string {
   s = s.replace(/[^a-z0-9-]+/g, '');
   s = s.length > 32 ? s.substr(0, 32) : s;
   return s;
+}
+
+/**
+ * Convert a phone number from E.164 format into (212)-555-9656 format
+ * @param phone - An unformatted 12-digit phone number string
+ */
+export function fromPhoneE164(phone: string): string {
+  if (phone.length === 12) {
+    return (
+      phone.substring(2, 5) +
+      '-' +
+      phone.substring(5, 8) +
+      '-' +
+      phone.substring(8, 12)
+    );
+  } else {
+    return phone;
+  }
+}
+
+/**
+ * Convert a phone number into E.164 format.
+ */
+export function toPhoneE164(phone: string): string {
+  phone = phone.replace(/[^0-9]/g, '');
+  if (phone.length === 10) {
+    phone = '1' + phone;
+  }
+  return '+' + phone;
 }
